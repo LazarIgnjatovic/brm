492c1e0223ac81ffe05f77c479b5a6321a2c97f9
LazarIgnjatovic <lazar.ignjatovic@elfak.rs>
2023-02-15 19:33:06 -0500

    cleanup

--- client.py
+++ client.py
@@ -2,15 +2,17 @@ import pandas as pd
 import grequests
 import json
 
+test_url = "http://localhost:8000/test"
+testfile_path = '/home/kali/brm/data/test/testset.csv'
 chunk_size = 10000
-data_chunks = pd.read_csv('/home/kali/brm/data/test/testset.csv',chunksize=chunk_size)
+data_chunks = pd.read_csv(testfile_path,chunksize=chunk_size)
 
 requests=[]
 for i, chunk in enumerate(data_chunks):
-    print(f"Sending request for chunk {i+1}...")
+    print(f"Forming request for chunk {i+1}...")
     test = chunk.drop([x for x in chunk.columns if 'Label_' in x], axis=1)
     json_data = test.to_json(orient='values')
-    requests.append(grequests.post("http://localhost:8000/test", json=json_data))
+    requests.append(grequests.post(test_url, json=json_data))
     if i>2:
         break
 
