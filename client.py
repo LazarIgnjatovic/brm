3afd4e77f586279b85387c31da72bc4c4659c18a
LazarIgnjatovic <lazar.ignjatovic@elfak.rs>
2023-02-16 10:54:46 -0500

    volumes

--- client.py
+++ client.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import grequests
 import json
+import logging
 
 test_url = "http://localhost:8000/test"
-testfile_path = '/home/kali/brm/data/test/testset.csv'
+testfile_path = '/home/kali/RUO/data/test/testset.csv'
 chunk_size = 10000
 data_chunks = pd.read_csv(testfile_path,chunksize=chunk_size)
+logging.basicConfig(level=logging.INFO)
+
+#NOTE: Model has to be present inside of the docker volume used by containers
 
 requests=[]
 for i, chunk in enumerate(data_chunks):
-    print(f"Forming request for chunk {i+1}...")
+    logging.info(f"Forming request for chunk {i+1}...")
     test = chunk.drop([x for x in chunk.columns if 'Label_' in x], axis=1)
     json_data = test.to_json(orient='values')
     requests.append(grequests.post(test_url, json=json_data))
-    if i>2:
+    if i>20:
         break
 
 for resp in grequests.imap(requests, size=10):
-    print(resp)
+    logging.info(resp)
