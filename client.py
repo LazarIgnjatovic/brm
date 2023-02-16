08724318a2534cfd6388a67c606a2999989a922c
LazarIgnjatovic <lazar.ignjatovic@elfak.rs>
2023-02-15 19:30:31 -0500

    request kako valja i trebuje

--- client.py
+++ client.py
@@ -1,13 +1,18 @@
 import pandas as pd
-import requests
+import grequests
+import json
 
-chunk_size = 1000
+chunk_size = 10000
 data_chunks = pd.read_csv('/home/kali/brm/data/test/testset.csv',chunksize=chunk_size)
 
+requests=[]
 for i, chunk in enumerate(data_chunks):
     print(f"Sending request for chunk {i+1}...")
-    test=chunk.drop([x for x in chunk.columns if 'Label_' in x], axis=1)
+    test = chunk.drop([x for x in chunk.columns if 'Label_' in x], axis=1)
     json_data = test.to_json(orient='values')
-    response = requests.post("http://localhost:8000/test", json=json_data)
-    print(response.text)
-    break
+    requests.append(grequests.post("http://localhost:8000/test", json=json_data))
+    if i>2:
+        break
+
+for resp in grequests.imap(requests, size=10):
+    print(resp)
--- server.py
+++ server.py
@@ -12,29 +12,17 @@ if __name__ != '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-model = None
 model_path = os.environ.get("MODEL_PATH", "/app/model")
 
 def load_model(model_path):
-    global model
-    app.logger.info('Loading model...')
     model = tf.keras.models.load_model(model_path)
-    app.logger.info('Model loaded!')
+    return model
 
 @app.route("/test", methods=["POST"])
 def test_model():
-    load_model(model_path)
+    model = load_model(model_path)
     csv = request.get_json()
     array = json.loads(csv)
     data = np.array(array, np.float64)
     result = model.predict(data)
     return str(result.tolist())
-
-# @app.route("/reload", methods=["POST"])
-# def reload_model():
-#     load_model(model_path)
-#     return "Model reloaded successfully."
-
-# if __name__ == "__main__":
-#     load_model(model_path)
-#     app.run(debug=True)
