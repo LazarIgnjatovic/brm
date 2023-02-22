1961f33429011b0e0f2b49dca1291e4a65cd9c79
LazarIgnjatovic <lazar.ignjatovic@elfak.rs>
2023-02-21 20:24:58 -0500

    radi

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import grequests
 import json
 import logging
 
-test_url = "http://localhost:8000/test"
+test_url = "http://localhost:80/test"
 testfile_path = '/home/kali/RUO/data/test/testset.csv'
 chunk_size = 10000
 data_chunks = pd.read_csv(testfile_path,chunksize=chunk_size)
@@ -17,7 +17,7 @@ for i, chunk in enumerate(data_chunks):
     test = chunk.drop([x for x in chunk.columns if 'Label_' in x], axis=1)
     json_data = test.to_json(orient='values')
     requests.append(grequests.post(test_url, json=json_data))
-    if i>20:
+    if i>3:
         break
 
 for resp in grequests.imap(requests, size=10):
